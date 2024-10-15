8b632107a40b10a106bba84f4218f948726cbde6
Administrator <Administrator@PC-40>
2024-10-15 10:00:22 +0900

    ch06

---
+++ ch06/src/ch06Main/Car.java
@@ -0,0 +1,21 @@
+package ch06Main;
+
+public class Car {
+	String company;
+	String model;
+	
+	int maxSpeed = 100;
+	int speed = 60;
+	
+	void ShowMaxSpeed()
+	{
+		System.out.println("Max Speed : " + maxSpeed);
+	}
+	
+	void ShowSpeed()
+	{
+		System.out.println("speed : " + speed);
+	}
+	
+
+}
---
+++ ch06/src/ch06Main/Main.java
@@ -0,0 +1,11 @@
+package ch06Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		car.ShowMaxSpeed();
+		car.ShowSpeed();
+	}
+
+}
