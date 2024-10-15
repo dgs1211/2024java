1ce864f0239c557b4b6e7950879bacb0b868d61b
Administrator <Administrator@PC-40>
2024-10-15 10:31:16 +0900

    update

--- ch06/src/ch06Main/Car.java
+++ ch06/src/ch06Main/Car.java
@@ -4,18 +4,29 @@ public class Car {
 	String company;
 	String model;
 	
-	int maxSpeed = 100;
-	int speed = 60;
+	int maxSpeed;
+	int speed ;
 	
-	void ShowMaxSpeed()
+	Car(String company, String model, int maxSpeed, int speed){
+		this.company = company;
+		this.model = model;
+		this.maxSpeed = maxSpeed;
+		this.speed = speed;
+	}
+	
+	Car(){
+		company = "";
+		model = "";
+		maxSpeed = 0;
+		speed = 0;
+	}
+	
+	void ShowInfo()
 	{
+		System.out.println("Company : " + company);
+		System.out.println("Model : " + model);
 		System.out.println("Max Speed : " + maxSpeed);
+		System.out.println("Speed : " + speed);
 	}
-	
-	void ShowSpeed()
-	{
-		System.out.println("speed : " + speed);
-	}
-	
 
 }
--- ch06/src/ch06Main/Main.java
+++ ch06/src/ch06Main/Main.java
@@ -3,9 +3,13 @@ package ch06Main;
 public class Main {
 
 	public static void main(String[] args) {
-		Car car = new Car();
-		car.ShowMaxSpeed();
-		car.ShowSpeed();
+		Car car1 = new Car("Hyundai", "Sonata", 100, 0);
+		Car car2 = new Car("Bents", "S100", 200, 0);
+		Car car3 = new Car();
+		
+		car1.ShowInfo();
+		car2.ShowInfo();
+		car3.ShowInfo();
 	}
 
 }
